1f9a93191860bbe05598dc6d297244ce7be1b651
Nadav Dayan <ndayan@appdev.yearup.org>
2023-06-28 15:22:53 -0400

    Test configuration patched up with @PropertySource(classpath:application.properties), created categoryDao Unit test

--- capstone-starter/src/test/java/org/yearup/data/mysql/MySqlCategoryDaoTest.java
+++ capstone-starter/src/test/java/org/yearup/data/mysql/MySqlCategoryDaoTest.java
@@ -2,7 +2,6 @@ package org.yearup.data.mysql;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.yearup.configuration.TestDatabaseConfig;
 import org.yearup.models.Category;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,20 +11,28 @@ class MySqlCategoryDaoTest extends BaseDaoTestClass {
     private MySqlCategoryDao dao;
 
     @BeforeEach
-    public void setUp() { dao = new MySqlCategoryDao(dataSource);}
+    public void setUp() {
+        dao = new MySqlCategoryDao(dataSource);
+    }
 
     @Test
     public void getById_shouldReturn_correctCategory () {
 
         //arrange
         int categoryId = 3;
-        Category expected = new Category();
+
+        Category expected = new Category()
         {{
-            
-        }}
+            setCategoryId(3);
+            setName("Home & Kitchen");
+            setDescription("Find everything you need to decorate and equip your home.");
+        }};
 
         // act
+        var actual = dao.getById(categoryId);
+
         // assert
+        assertEquals(expected.getName(), actual.getName(), "Just to make sure the ID entered is the ID expected");
     }
 
 }
